0b9ad736ec88dbda9d2e11155b6fc03bb6d6a77b
NicolasLopez147 <nilopezn@unal.edu.co>
2023-10-01 12:59:49 -0500

    Implementation of schema zod for products

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import { authRequired } from '../middlewares/validateUser.middlewares';
 import  productController from "../controllers/products.controller";
-// import { validateSchema } from "../middlewares/validator.middleware";
-// import { loginSchema, registerSchema,  } from "../schemas/auth.schema";
+import { validateSchema } from "../middlewares/validator.middleware";
+import { productSchema } from "../schemas/product.schema";
 
 const productRouter = Router();
 
 productRouter.get("/",authRequired,productController.getProducts);
 productRouter.get("/:id",authRequired,productController.getProduct);
-productRouter.post("/",authRequired,productController.createProduct);
+productRouter.post("/",authRequired,validateSchema(productSchema),productController.createProduct);
 productRouter.delete("/:id",authRequired,productController.deleteProduct);
-productRouter.put("/:id",authRequired,productController.updateProduct);
+productRouter.put("/:id",authRequired,validateSchema(productSchema),productController.updateProduct);
 
 export default productRouter;
