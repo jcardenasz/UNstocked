58bef1aa1b4d2baae23e994d9941acf155157323
Juan Camilo Cardenas Zabala <jcardenasz@unal.edu.co>
2023-10-18 12:39:18 -0500

    mailer services updated

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,6 @@ const app = new App();
 const config = new Config;
 void connectDB();
 app.getApp().listen(config.getPORT(), () => {
+	// eslint-disable-next-line no-console
 	console.log('Server port ', config.getPORT());
 });
--- src/services/mailer.services.ts
+++ src/services/mailer.services.ts
@@ -20,7 +20,7 @@ export class MailerService {
 			subject: 'Reset Password',
 			html: `<p>You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
 			Please click on the following link, or paste this into your browser to complete the process:\n\n
-			http://localhost:3000/reset/${token}\n\n
+			http://localhost:3000/resetPassword/${token}\n\n
 			If you did not request this, please ignore this email and your password will remain unchanged.\n</p>`
 		};
 		await transporter.sendMail(mailOptions);
