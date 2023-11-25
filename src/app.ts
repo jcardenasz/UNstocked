04eacd8ef838a5e5280158b2028ea7f934390405
Juan Camilo Cardenas Zabala <jcardenasz@unal.edu.co>
2023-11-25 15:59:02 -0500

    multiple origins cors app.ts

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,15 @@ export class App {
 	private initMiddlewares(): void {
 		this.app.use(morgan('dev'));
 		this.app.use(cors({
-			origin: 'http://localhost:3000', // replace with your frontend origin
+			// origin: 'http://localhost:3000',
+			origin: (origin, callback) => {
+				const allowedOrigins = ['http://localhost:3000', 'https://u-nstocked-bmb5.vercel.app/api'];
+				if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+					callback(null, true);
+				} else {
+					callback(new Error('Not allowed by CORS'));
+				}
+			},
 			methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT', 'OPTIONS'],
 			allowedHeaders: ['Content-Type', 'Authorization'],
 			credentials: true,
