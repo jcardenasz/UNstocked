826da7a34f113bc4e2f5aa899670e2c9d272d7af
Juan Camilo Cardenas Zabala <jcardenasz@unal.edu.co>
2023-11-25 19:13:23 -0500

    multiple cors origins

--- src/app.ts
+++ src/app.ts
@@ -20,15 +20,15 @@ export class App {
 		this.app.use(morgan('dev'));
 		this.app.use(cors({
 			// origin: 'http://localhost:3000',
-			origin: 'https://u-nstocked-bmb5.vercel.app/api',
-			// origin: (origin, callback) => {
-			// 	const allowedOrigins = ['http://localhost:3000', 'https://u-nstocked-bmb5.vercel.app/api'];
-			// 	if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-			// 		callback(null, true);
-			// 	} else {
-			// 		callback(new Error('Not allowed by CORS'));
-			// 	}
-			// },
+			// origin: 'https://u-nstocked-bmb5.vercel.app/api',
+			origin: (origin, callback) => {
+				const allowedOrigins = ['http://localhost:3000', 'https://u-nstocked-bmb5.vercel.app/api'];
+				if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+					callback(null, true);
+				} else {
+					callback(new Error('Not allowed by CORS'));
+				}
+			},
 			methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT', 'OPTIONS'],
 			allowedHeaders: ['Content-Type', 'Authorization'],
 			credentials: true,
